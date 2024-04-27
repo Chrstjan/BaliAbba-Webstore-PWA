d89f2209321665588e7e3fe59fa3010cb2ceab05
Christian <ps5christian@hotmail.com>
2024-04-27 21:08:50 +0200

    Sorted the categories from dummyjson into sup categories with sub categories

--- assets/js/Components/categories/getCategories.js
+++ assets/js/Components/categories/getCategories.js
@@ -1,7 +1,8 @@
 import { myFetchData } from "../../Utils/apiUtils.js";
+import { receivedCategories } from "./receivedCategories.js";
 
 export const getProductCategories = async () => {
   const categoriesEndpoint = `https://dummyjson.com/products/categories?limit=0`;
   const categoriesData = await myFetchData(categoriesEndpoint);
-  console.log(categoriesData);
+  receivedCategories(categoriesData);
 };
---
+++ assets/js/Components/categories/receivedCategories.js
@@ -0,0 +1,89 @@
+let eletronicArray = [];
+let homeDecorArray = [];
+let clothesArray = [];
+let accesoriesArray = [];
+let skincareArray = [];
+let vehiclesArray = [];
+let miscArray = [];
+
+export const receivedCategories = (categories) => {
+  console.log(categories);
+
+  categories.map((category) => {
+    switch (category) {
+      case "smartphones":
+      case "laptops":
+        eletronicArray.push(category);
+        break;
+
+      case "home-decoration":
+      case "furniture":
+      case "lighting":
+        homeDecorArray.push(category);
+        break;
+
+      case "tops":
+      case "womens-dresses":
+      case "womens-shoes":
+      case "mens-shirts":
+      case "mens-shoes":
+        clothesArray.push(category);
+        break;
+
+      case "mens-watches":
+      case "womens-watches":
+      case "womens-bags":
+      case "womens-jewellery":
+      case "sunglasses":
+        accesoriesArray.push(category);
+        break;
+
+      case "fragrances":
+      case "skincare":
+        skincareArray.push(category);
+        break;
+
+      case "automotive":
+      case "motorcycle":
+        vehiclesArray.push(category);
+        break;
+
+      default:
+        miscArray.push(category);
+        break;
+    }
+  });
+
+  let supCategoryData = [
+    {
+      supCategory: "Eletronic Devices",
+      subCategory: eletronicArray,
+    },
+    {
+      supCategory: "Home Decoration",
+      subCategory: homeDecorArray,
+    },
+    {
+      supCategory: "Clothes",
+      subCategory: clothesArray,
+    },
+    {
+      supCategory: "Accesories",
+      subCategory: accesoriesArray,
+    },
+    {
+      supCategory: "Skin Care",
+      subCategory: skincareArray,
+    },
+    {
+      supCategory: "Vehicles",
+      subCategory: vehiclesArray,
+    },
+    {
+      supCategory: "Misc",
+      subCategory: miscArray,
+    },
+  ];
+
+  console.log(supCategoryData);
+};
