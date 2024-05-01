5381473d050585b515c667e24bab7d994b0d1315
Christian <ps5christian@hotmail.com>
2024-05-01 16:13:35 +0200

    Started making the code to append the categories html elements to the dom

--- assets/js/Components/categories/buildCategoriesCards.js
+++ assets/js/Components/categories/buildCategoriesCards.js
@@ -1,3 +1,27 @@
+const categoryParentContainer = document.getElementById("app");
+
 export const buildCategoriesCard = (categories) => {
+  clearApp();
   console.log(categories);
+
+  const categoryContainer = document.createElement("div");
+  categoryContainer.classList.add("category-container");
+
+  let supCategoryHeader = `
+    <header>
+      <h2>${categories.supCategory}</h2>
+    </header>`;
+
+  categoryContainer.innerHTML += supCategoryHeader;
+
+  categories.subCategory.map((subCategories) => {
+    console.log(subCategories);
+    let subCategoriesBtn = `<button>${subCategories}</button>`;
+
+    categoryContainer.innerHTML += subCategoriesBtn;
+  });
+};
+
+const clearApp = () => {
+  categoryParentContainer.innerHTML = "";
 };
